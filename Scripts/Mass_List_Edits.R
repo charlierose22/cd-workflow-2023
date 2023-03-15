28e65e856d39a4d477f84efdc907a09ec94c7df6
Charlotte Head <crh552@york.ac.uk>
2023-03-15 12:38:05 +0000

    update mass lists

---
+++ Scripts/Mass_List_Edits.R
@@ -0,0 +1,21 @@
+# bring data from mass list csv using janitor to clean names
+library(tidyverse)
+
+ITN <- read_csv("Data/Antibiotics_ITN_MSCA_ANSWER_160616_wDTXSIDs.csv") %>%
+  janitor::clean_names()
+ITN_Metabolites <- read_csv("Data/ITNANTIBIOTIC_CYP_Metabolites.csv") %>%
+  janitor::clean_names()
+Psychoactive <- read_csv("Data/KPS_Psychoactive_substances_v2.csv") %>%
+  janitor::clean_names()
+Pharmaceuticals <- read_csv("Data/KPS_Pharmaceuticals.csv") %>%
+  janitor::clean_names()
+
+# change column names in psychoactive to match for a large join. 
+colnames(Psychoactive)[2] = "name"
+
+# make sure each mass list has a column with which mass list each compound is from before we join together.
+ITN_ML <- add_column(ITN, mass_list = "ITN", .after = 0)
+ITN_Metabolites_ML <- add_column(ITN_Metabolites, mass_list = "ITN_Metabolites", .after = 0)
+Psychoactive_ML <- add_column(Psychoactive, mass_list = "Psychoactive", .after = 0)
+Pharmaceuticals_ML <- add_column(Pharmaceuticals, mass_list = "Pharmaceuticals", .after = 0)
+
